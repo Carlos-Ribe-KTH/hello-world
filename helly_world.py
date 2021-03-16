fffabe5d134b91fd56e8106a2fe1e127828d8ce0
Carlos-Ribe-KTH <carlossr@kth.se>
2021-03-16 19:56:37 +0100

    test_branch1

--- helly_world.py
+++ helly_world.py
@@ -1,3 +1,14 @@
 print("Hello World!")
 print("Kexjobb_2021")
 print("All your bases are belong to us")
+
+def print_hej():
+    print("Hej")
+
+
+#h
+print_hej()
+#f
+
+# Så! nu har jag skapat en branch via pycharm!
+# Hörs imorgon!
